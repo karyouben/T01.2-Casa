47cbf3b9f28d3072cc45e2d06675b0683ba1454b
willi <willi@DESKTOP-K0BDO30>
2022-02-15 17:55:51 +0100

    clase del 15/02/2022

---
+++ src/fp/figuras/test/TestCasa.java
@@ -0,0 +1,16 @@
+package fp.figuras.test;
+
+import fp.figuras.Casa;
+import fp.figuras.CasaImpl;
+
+public class TestCasa {
+
+	public static void main(String[] args) {
+		Casa c = new CasaImpl();
+		c.dibujar();
+		
+		c.setBlancoYNegro();
+		c.setColor();
+	}
+
+}
